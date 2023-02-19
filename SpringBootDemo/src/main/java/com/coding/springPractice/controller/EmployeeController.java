b9a0ce89da001792ae49668479c746d072c2770e
abhishek <abhishek.kumar7152@gmail.com>
2023-02-20 02:45:46 +0530

    Added some Rest methods

--- SpringBootDemo/src/main/java/com/coding/springPractice/controller/EmployeeController.java
+++ SpringBootDemo/src/main/java/com/coding/springPractice/controller/EmployeeController.java
@@ -2,12 +2,16 @@ package com.coding.springPractice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.coding.springPractice.Employee;
 import com.coding.springPractice.dao.EmployeeRepository;
+
+import jakarta.websocket.server.PathParam;
 
 
 @Controller
@@ -31,6 +35,30 @@ public class EmployeeController {
 		ModelAndView mv = new ModelAndView("showEmployee");
 		Employee employee= employeeRepo.findById(eid).orElse(new Employee());
 		mv.addObject("employee", employee);
+		
+//		just for experiment
+		System.out.println(employeeRepo.findAllBytech("java"));
+		System.out.println(employeeRepo.findByEidGreaterThan(102));
+		//System.out.println(employeeRepo.findByTechSorted("java"));//Using JPQL
+		
 		return mv;
 	}
+	
+	
+	@RequestMapping("/employees")
+	@ResponseBody
+	public String getEmployee() {
+		
+		return employeeRepo.findAll().toString();
+	}
+	
+
+	@RequestMapping("/employee/{eid}")
+	@ResponseBody
+	public String getEmployeeByID(@PathVariable int eid) {
+		
+		return employeeRepo.findById(eid).toString();
+	}
+	
+	
 }
--- SpringBootDemo/src/main/java/com/coding/springPractice/dao/EmployeeRepository.java
+++ SpringBootDemo/src/main/java/com/coding/springPractice/dao/EmployeeRepository.java
@@ -1,9 +1,19 @@
 package com.coding.springPractice.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.coding.springPractice.Employee;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
 
+	
+	List<Employee> findAllBytech(String string);
+	List<Employee> findByEidGreaterThan(Integer eid);
+
+	//JPQL
+	//@Query("from employee where tech=?1 order by ename")
+	//List<Employee> findByTechSorted(String tech);
 }
